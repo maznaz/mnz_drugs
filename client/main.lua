61c026d39f5b1aeac51481a8ef2920f5c8fb4a23
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-06 05:26:15 +0700

    update main.lua

--- client/main.lua
+++ client/main.lua
@@ -14,3 +14,76 @@ Citizen.CreateThread(function()
 
 	ESX.PlayerData = ESX.GetPlayerData()
 end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local playerPed = PlayerPedId()
+		local coords = GetEntityCoords(playerPed)
+
+		if GetDistanceBetweenCoords(coords, Config.CircleZones.DrugDealer.coords, true) < 0.5 then
+			if not menuOpen then
+				ESX.ShowHelpNotification(_U('dealer_prompt'))
+
+				if IsControlJustReleased(0, 38) then
+					wasOpen = true
+					OpenDrugShop()
+				end
+			else
+				Citizen.Wait(500)
+			end
+		else
+			if wasOpen then
+				wasOpen = false
+				ESX.UI.Menu.CloseAll()
+			end
+
+			Citizen.Wait(500)
+		end
+	end
+end)
+
+function OpenDrugShop()
+	ESX.UI.Menu.CloseAll()
+	local elements = {}
+	menuOpen = true
+
+	for k, v in pairs(ESX.GetPlayerData().inventory) do
+		local price = Config.DrugDealerItems[v.name]
+
+		if price and v.count > 0 then
+			table.insert(elements, {
+				label = ('%s - <span style="color:green;">%s</span>'):format(v.label, _U('dealer_item', ESX.Math.GroupDigits(price))),
+				name = v.name,
+				price = price,
+
+				-- menu properties
+				type = 'slider',
+				value = 1,
+				min = 1,
+				max = v.count
+			})
+		end
+	end
+
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'drug_shop', {
+		title    = _U('dealer_title'),
+		align    = 'top-left',
+		elements = elements
+	}, function(data, menu)
+		TriggerServerEvent('esx_drugs:sellDrug', data.current.name, data.current.value)
+	end, function(data, menu)
+		menu.close()
+		menuOpen = false
+	end)
+end
+
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		if menuOpen then
+			ESX.UI.Menu.CloseAll()
+		end
+	end
+end)
+
