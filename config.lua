3a7dbef7c13da9e912c946d0440b90b2897296a9
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-06 05:51:20 +0700

    update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ Config.ZoneSize     = {x= 00.0, y= 00.0, z= 00.0}
 Config.MarkColor    = {r= 00.0 g= 00.0 b= 00.0}
 Config.MarkType     = 1
 Config.Locale       = id
+Config.Cops         = 1
 
 Config.GiveBlack = true
 
