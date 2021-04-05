4fdc961e035d59e38bd06772af97bd75d68a8373
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-06 05:28:36 +0700

    update weed.lua

---
+++ client/weed.lua
@@ -0,0 +1,14 @@
+local spawnedWeeds = 0
+local weedPlants = {}
+local isPickingUp, isProcessing = false, false
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(500)
+		local coords = GetEntityCoords(PlayerPedId())
+
+		if GetDistanceBetweenCoords(coords, Config.CircleZones.WeedField.coords, true) < 50 then
+			SpawnWeedPlants()
+		end
+	end
+end)
