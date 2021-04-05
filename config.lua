d030134a2d375c4e3c0af6aaf6aee759200fde8e
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-05 18:23:15 +0700

    update config.lua

--- config.lua
+++ config.lua
@@ -2,3 +2,24 @@ Config              = {}
 Config.DrawDistance = 100.0
 Config.ZoneSize     = {x= 00.0, y= 00.0, z= 00.0}
 Config.MarkColor    = {r= 00.0 g= 00.0 b= 00.0}
+Config.MarkType     = 1
+Config.Locale       = id
+Config.Zone = {
+    {x= 00.0 y= 00.0 z= 00.0}
+}
+
+Config.Location = {
+    Ladang = vector3(00.0, 00.0, 00.0),
+    Ladang_kedua = vector3(11.1, 11.1, 11.1)
+}
+Config.ObjLocations = {
+    Ladang  = {x = 00.0, y = 00.0, z = 00.0},
+    Ladang_kedua = {x = 11.1, y = 11.1, z = 11.1}
+}
+
+Config.Price = {
+    weed    = 1000,
+    opium   = 1000,
+    lsd     = 1000,
+    coke    = 1000,
+}
