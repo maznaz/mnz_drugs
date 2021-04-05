72c29caac5d9d856164123a27c2c081653b62cac
M_N•Script <67985905+maznaz@users.noreply.github.com>
2021-04-06 05:15:54 +0700

    update main.lua

--- client/main.lua
+++ client/main.lua
@@ -11,3 +11,23 @@ local Keys = {
 	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
 	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
+
+local PlayerData                = {}
+local GUI                       = {}
+local HasAlreadyEnteredMarker   = false
+local LastZone                  = nil
+local CurrentAction             = nil
+local CurrentActionMsg          = ''
+local CurrentActionData         = {}
+ESX                             = nil
+GUI.Time                        = 0
+
+Citizen.CreateThread(function()
+
+	while ESX == nil do
+
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
